ea284bc1af0003a5f60d29a4d3cacc37dcaf91fd
Pankaj Nandlal Sharma <pankajnandlalsharma@gmail.com>
2026-07-12 21:51:16 +0530

    KE-002 Knowledge Repository Engine

---
+++ src/engine/KnowledgeRepositoryEngine.js
@@ -0,0 +1,64 @@
+import Enterprise from "../models/Enterprise";
+
+export default class KnowledgeRepositoryEngine {
+
+    add(document) {
+
+        Enterprise.knowledge.documents.push(document);
+
+        return document;
+
+    }
+
+    getAll() {
+
+        return Enterprise.knowledge.documents;
+
+    }
+
+    get(id) {
+
+        return Enterprise.knowledge.documents.find(
+            document => document.id === id
+        );
+
+    }
+
+    update(id, updates) {
+
+        const document = this.get(id);
+
+        if (!document) return null;
+
+        Object.assign(document, updates);
+
+        document.lastUpdated = new Date();
+
+        return document;
+
+    }
+
+    remove(id) {
+
+        Enterprise.knowledge.documents =
+            Enterprise.knowledge.documents.filter(
+                document => document.id !== id
+            );
+
+    }
+
+    count() {
+
+        return Enterprise.knowledge.documents.length;
+
+    }
+
+    exists(id) {
+
+        return Enterprise.knowledge.documents.some(
+            document => document.id === id
+        );
+
+    }
+
+}
