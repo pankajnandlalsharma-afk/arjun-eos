acb6d4d251a0e558e07c1debe42b5c1e6cc5c30a
Pankaj Nandlal Sharma <pankajnandlalsharma@gmail.com>
2026-07-15 17:05:16 +0530

    Sprint-014 Pattern Intelligence Engine

--- src/feature/PatternIntelligenceEngine.js
+++ src/feature/PatternIntelligenceEngine.js
@@ -10,29 +10,65 @@ export default class PatternIntelligenceEngine {
 
             patternBlueprint: {
 
-                viralPatterns: [],
+                viralPatterns:
+                    this.getViralPatterns(
+                        researchBlueprint
+                    ),
 
-                hookPatterns: [],
+                hookPatterns:
+                    this.getHookPatterns(
+                        researchBlueprint
+                    ),
 
-                titlePatterns: [],
+                titlePatterns:
+                    this.getTitlePatterns(
+                        researchBlueprint
+                    ),
 
-                thumbnailPatterns: [],
+                thumbnailPatterns:
+                    this.getThumbnailPatterns(
+                        researchBlueprint
+                    ),
 
-                scriptPatterns: [],
+                scriptPatterns:
+                    this.getScriptPatterns(
+                        researchBlueprint
+                    ),
 
-                storytellingPatterns: [],
+                storytellingPatterns:
+                    this.getStorytellingPatterns(
+                        researchBlueprint
+                    ),
 
-                engagementPatterns: [],
+                engagementPatterns:
+                    this.getEngagementPatterns(
+                        researchBlueprint
+                    ),
 
-                postingPatterns: [],
+                postingPatterns:
+                    this.getPostingPatterns(
+                        researchBlueprint
+                    ),
 
-                audiencePatterns: [],
+                audiencePatterns:
+                    this.getAudiencePatterns(
+                        researchBlueprint
+                    ),
 
-                revenuePatterns: [],
+                revenuePatterns:
+                    this.getRevenuePatterns(
+                        researchBlueprint
+                    ),
 
-                repeatableFrameworks: [],
+                repeatableFrameworks:
+                    this.getRepeatableFrameworks(
+                        researchBlueprint
+                    ),
 
-                confidenceScore: 0
+                confidenceScore:
+                    this.calculateConfidenceScore(
+                        researchBlueprint
+                    )
 
             },
 
@@ -43,6 +79,94 @@ export default class PatternIntelligenceEngine {
             nextStep: "Trajectory Intelligence Engine"
 
         };
+
+    }
+
+    getViralPatterns(researchBlueprint) {
+
+        return researchBlueprint.viralPatterns || [];
+
+    }
+
+    getHookPatterns(researchBlueprint) {
+
+        return researchBlueprint.hookPatterns || [];
+
+    }
+
+    getTitlePatterns(researchBlueprint) {
+
+        return researchBlueprint.titlePatterns || [];
+
+    }
+
+    getThumbnailPatterns(researchBlueprint) {
+
+        return researchBlueprint.thumbnailPatterns || [];
+
+    }
+
+    getScriptPatterns(researchBlueprint) {
+
+        return researchBlueprint.scriptPatterns || [];
+
+    }
+
+    getStorytellingPatterns(researchBlueprint) {
+
+        return researchBlueprint.storytellingPatterns || [];
+
+    }
+
+    getEngagementPatterns(researchBlueprint) {
+
+        return researchBlueprint.engagementPatterns || [];
+
+    }
+
+    getPostingPatterns(researchBlueprint) {
+
+        return researchBlueprint.postingPatterns || [];
+
+    }
+
+    getAudiencePatterns(researchBlueprint) {
+
+        return researchBlueprint.audiencePatterns || [];
+
+    }
+
+    getRevenuePatterns(researchBlueprint) {
+
+        return researchBlueprint.revenuePatterns || [];
+
+    }
+
+    getRepeatableFrameworks(researchBlueprint) {
+
+        return researchBlueprint.repeatableFrameworks || [];
+
+    }
+
+    calculateConfidenceScore(researchBlueprint) {
+
+        let score = 0;
+
+        score += (researchBlueprint.youtubeResearch || []).length;
+
+        score += (researchBlueprint.googleResearch || []).length;
+
+        score += (researchBlueprint.newsResearch || []).length;
+
+        score += (researchBlueprint.booksResearch || []).length;
+
+        score += (researchBlueprint.papersResearch || []).length;
+
+        score += (researchBlueprint.trendingTopics || []).length;
+
+        score += (researchBlueprint.emergingTopics || []).length;
+
+        return score;
 
     }
 
