62de1334ea9a0074990bd581a98ea8010ba8d041
Pankaj Nandlal Sharma <pankajnandlalsharma@gmail.com>
2026-07-17 16:11:04 +0530

    Sprint-019 Task-004: Create Model Exports

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+/**
+ * ====================================================
+ * ARJUN EOS
+ * Quiz Domain Models
+ *
+ * Central export point for all models.
+ * ====================================================
+ */
+
+export { default as Quiz } from "./Quiz";
+export { default as Question } from "./Question";
+export { default as Option } from "./Option";
