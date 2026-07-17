b6540111ad71ff084d11a74437a89c47bdf8e65e
Pankaj Nandlal Sharma <pankajnandlalsharma@gmail.com>
2026-07-17 16:24:27 +0530

    Sprint-020 Task-002: Create Controller Exports

---
+++ src/controllers/index.js
@@ -0,0 +1,10 @@
+/**
+ * ====================================================
+ * ARJUN EOS
+ * Controller Exports
+ *
+ * Central export point for all controllers.
+ * ====================================================
+ */
+
+export { default as QuizController } from "./QuizController";
