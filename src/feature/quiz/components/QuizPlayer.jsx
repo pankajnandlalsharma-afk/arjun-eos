bed1903949ebb43c2169b94b82da735e4ec43217
Pankaj Nandlal Sharma <pankajnandlalsharma@gmail.com>
2026-07-25 22:19:11 +0530

    Sprint-044: Enhanced Quiz Player

--- src/feature/quiz/components/QuizPlayer.jsx
+++ src/feature/quiz/components/QuizPlayer.jsx
@@ -12,13 +12,114 @@ function QuizPlayer({
     onExit
 }) {
 
-    const [currentQuestion] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [selectedAnswers, setSelectedAnswers] = useState({});
+    const [showResult, setShowResult] = useState(false);
 
     if (!quiz) {
         return null;
     }
 
-    const question = quiz.questions?.[currentQuestion];
+    const questions = quiz.questions || [];
+    const question = questions[currentQuestion];
+
+    const handleSelect = (optionId) => {
+
+        setSelectedAnswers({
+            ...selectedAnswers,
+            [currentQuestion]: optionId
+        });
+
+    };
+
+    const nextQuestion = () => {
+
+        if (currentQuestion < questions.length - 1) {
+
+            setCurrentQuestion(currentQuestion + 1);
+
+        } else {
+
+            setShowResult(true);
+
+        }
+
+    };
+
+    const previousQuestion = () => {
+
+        if (currentQuestion > 0) {
+
+            setCurrentQuestion(currentQuestion - 1);
+
+        }
+
+    };
+
+    const calculateScore = () => {
+
+        let score = 0;
+
+        questions.forEach((q, index) => {
+
+            if (selectedAnswers[index] === q.correctOptionId) {
+
+                score++;
+
+            }
+
+        });
+
+        return score;
+
+    };
+
+    if (showResult) {
+
+        return (
+
+            <div
+                style={{
+                    padding: "25px",
+                    border: "1px solid #d1d5db",
+                    borderRadius: "10px"
+                }}
+            >
+
+                <h2>Quiz Completed</h2>
+
+                <h3>
+
+                    Score : {calculateScore()} / {questions.length}
+
+                </h3>
+
+                <button
+                    onClick={() => {
+
+                        setCurrentQuestion(0);
+                        setSelectedAnswers({});
+                        setShowResult(false);
+
+                    }}
+                >
+                    Restart Quiz
+                </button>
+
+                <button
+                    style={{
+                        marginLeft: "10px"
+                    }}
+                    onClick={onExit}
+                >
+                    Exit Quiz
+                </button>
+
+            </div>
+
+        );
+
+    }
 
     return (
 
@@ -33,7 +134,9 @@ function QuizPlayer({
             <h2>{quiz.title}</h2>
 
             <p>
-                Question {currentQuestion + 1} of {quiz.questions.length}
+
+                Question {currentQuestion + 1} of {questions.length}
+
             </p>
 
             <hr />
@@ -41,30 +144,42 @@ function QuizPlayer({
             {question ? (
 
                 <>
+
                     <h3>{question.question}</h3>
 
-                    {(question.options || []).map((option) => (
+                    {(question.options || []).map((option) => {
 
-                        <div
-                            key={option.id}
-                            style={{
-                                marginTop: "12px"
-                            }}
-                        >
+                        const selected =
+                            selectedAnswers[currentQuestion] === option.id;
+
+                        return (
 
                             <button
+                                key={option.id}
+                                onClick={() => handleSelect(option.id)}
                                 style={{
+                                    display: "block",
                                     width: "100%",
                                     textAlign: "left",
-                                    padding: "10px"
+                                    padding: "12px",
+                                    marginTop: "12px",
+                                    cursor: "pointer",
+                                    borderRadius: "6px",
+                                    border: "1px solid #ccc",
+                                    background: selected
+                                        ? "#dbeafe"
+                                        : "#ffffff"
                                 }}
                             >
+
                                 {option.text}
+
                             </button>
 
-                        </div>
+                        );
 
-                    ))}
+                    })}
+
                 </>
 
             ) : (
@@ -75,7 +190,42 @@ function QuizPlayer({
 
             <div
                 style={{
-                    marginTop: "25px"
+                    marginTop: "25px",
+                    display: "flex",
+                    justifyContent: "space-between"
+                }}
+            >
+
+                <button
+                    disabled={currentQuestion === 0}
+                    onClick={previousQuestion}
+                >
+                    Previous
+                </button>
+
+                {currentQuestion < questions.length - 1 ? (
+
+                    <button
+                        onClick={nextQuestion}
+                    >
+                        Next
+                    </button>
+
+                ) : (
+
+                    <button
+                        onClick={nextQuestion}
+                    >
+                        Finish Quiz
+                    </button>
+
+                )}
+
+            </div>
+
+            <div
+                style={{
+                    marginTop: "20px"
                 }}
             >
 
